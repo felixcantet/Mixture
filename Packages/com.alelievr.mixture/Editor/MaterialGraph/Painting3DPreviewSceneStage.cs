4973ebc90172190e435c17bed93f360dff35743c
Félix <felixcantet@gmail.com>
2021-06-08 19:05:50 +0200

    Basic Paint Node setup

--- Packages/com.alelievr.mixture/Editor/MaterialGraph/Painting3DPreviewSceneStage.cs
+++ Packages/com.alelievr.mixture/Editor/MaterialGraph/Painting3DPreviewSceneStage.cs
@@ -1,11 +1,56 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
+using UnityEditor.EditorTools;
+using UnityEditor.Graphs;
 using UnityEditor.SceneManagement;
+using UnityEditor.UIElements;
 using UnityEngine;
+using UnityEngine.Rendering.VirtualTexturing;
 
 namespace Mixture
 {
+    public class PaintWindow : EditorWindow
+    {
+        
+    }
+    
+    [CustomEditor(typeof(PaintTarget))]
+    public class TestPaintGUI : Editor
+    {
+        private int selectedIndex = 0;
+        private Material mat;
+        private void OnEnable()
+        {
+            Debug.Log("Enable GUI");
+            mat = new Material(Shader.Find("Standard"));
+        }
+
+        private void OnSceneGUI()
+        {
+            var go = (target as PaintTarget).gameObject; 
+            HandleUtility.AddDefaultControl(GUIUtility.GetControlID(FocusType.Passive));
+            //SceneView.lastActiveSceneView.drawGizmos = false;
+            SceneView.lastActiveSceneView.LookAt(go.transform.position);
+            var pos = Event.current.mousePosition;
+
+            if (Event.current.isMouse && Event.current.type == EventType.MouseDown)
+            {
+                Debug.Log("Mouse Event");
+                // Paint On Mouse Down
+            }
+            
+            Handles.BeginGUI();
+            
+            selectedIndex = GUILayout.Toolbar(selectedIndex,
+                new GUIContent[]{new GUIContent(AssetPreview.GetAssetPreview(mat)), new GUIContent("X"), new GUIContent("B"), MixtureToolbar.Styles.settingsIcon}, GUI.skin.button , GUILayout.Width(50 * 4), GUILayout.Height(50));
+            //selectedIndex = GUILayout.Toolbar(selectedIndex, new string[3]{"Test", "X", "B"}, GUILayout.Width(50), MixtureToolbar.Styles.settingsIcon));
+            Handles.EndGUI();
+        }
+        
+    }
+    
     public class Painting3DPreviewSceneStage : PreviewSceneStage
     {
         // Récupérer un mesh
@@ -45,6 +90,8 @@ namespace Mixture
             
             MeshRenderer rd = test.AddComponent<MeshRenderer>();
             rd.sharedMaterial = refMat;
+
+            test.AddComponent<PaintTarget>();
             
             EditorSceneManager.MoveGameObjectToScene(test, scene);
         }
@@ -54,15 +101,17 @@ namespace Mixture
             var baseOpenStage = base.OnOpenStage();
 
             Debug.Log("We are opening a stage !");
-
+            HandleUtility.AddDefaultControl(GUIUtility.GetControlID(FocusType.Passive));
             return baseOpenStage;
         }
 
         protected override void OnCloseStage()
         {
             base.OnCloseStage();
-            
+            HandleUtility.AddDefaultControl(GUIUtility.GetControlID(FocusType.Keyboard));
             Debug.Log("We closed the stage");
         }
+        
+        
     }
 }
